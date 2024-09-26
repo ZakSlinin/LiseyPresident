60a84985bc10ebd09de474aaf6bf07590506c634
ZakSlinin <156526564+ZakSlinin@users.noreply.github.com>
2024-09-26 20:39:15 +0300

    add main page, addgetCandidate

---
+++ mainPages/getCandidate.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const candidatesContainer = document.getElementById('candidates-container');
+
+    fetch('https://www.kringeproduction.ru/candidates/')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            data.forEach(candidate => {
+                const candidateDiv = document.createElement('div');
+                candidateDiv.classList.add('candidate-background');
+
+                const photoDiv = document.createElement('div');
+                photoDiv.classList.add('candidate-photo');
+                const img = document.createElement('img');
+                img.src = itemPipe(candidate.photo);
+                photoDiv.appendChild(img);
+
+                const buttonNameDiv = document.createElement('div');
+                buttonNameDiv.classList.add('candidate-button-name');
+                const nameH1 = document.createElement('h1');
+                nameH1.classList.add('text-candidate');
+                nameH1.textContent = candidate.name;
+
+                const button = document.createElement('button');
+                button.classList.add('to-know');
+                button.textContent = 'Узнать';
+
+                buttonNameDiv.appendChild(nameH1);
+                buttonNameDiv.appendChild(button);
+
+                candidateDiv.appendChild(photoDiv);
+                candidateDiv.appendChild(buttonNameDiv);
+
+                candidatesContainer.appendChild(candidateDiv);
+            });
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+});
+
+function itemPipe(data) {
+    let item = data;
+    if (item && item.startsWith('http://127.0.0.1:8001/')) {
+        item = item.replace('http://127.0.0.1:8001/', 'https://www.kringeproduction.ru/files/');
+    }
+    return item;
+}
---
+++ mainPages/main.ts
@@ -0,0 +1,6 @@
+const candidate = document.getElementById('candidate');
+const candidate2 = document.getElementById('candidate2');
+
+if (candidate.onclick) {
+    localStorage.setItem('nameOfCandidate', 'чел')
+}
